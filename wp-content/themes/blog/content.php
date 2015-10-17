5f040ea0d8056103a5d3fc0dad35a8e589359ce3
annaosk <a.sebetic@gmail.com>
2015-10-17 17:08:24 +0200

    initial commit

---
+++ wp-content/themes/blog/content.php
@@ -0,0 +1,11 @@
+<article class="single-post">
+	<?php the_post_thumbnail('post_image'); ?>
+    <h2>
+            <?php the_title(); ?>
+    </h2>
+
+    <p>
+        <?php the_content(); ?>
+    </p>
+
+</article>
---
+++ wp-content/themes/blog/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+<link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,600,700' rel='stylesheet' type='text/css'> 
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+        <nav class="site-nav">
+            <button class="close-btn">X</button>
+            <?php
+            $args = array(
+                'theme_location' => 'primary'
+            );
+
+            wp_nav_menu($args);
+            ?>
+        </nav>  
+        <!-- header -->
+        <header>
+            <div class="logo">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png"></a>
+            </div>
+            <button class="menu-btn">Menu</button>
+            <nav class="nav">
+            <?php
+            $args = array(
+                'theme_location' => 'primary'
+            );
+
+            wp_nav_menu($args);
+            ?>
+            </nav>              
+        </header>
+    <!-- Container -->
+    <div class="container">

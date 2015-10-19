09dc01d0674617d4450654c9ec4bc1e82bd3b507
annaosk <a.sebetic@gmail.com>
2015-10-19 12:09:23 +0200

    change theme name

---
+++ wp-content/themes/imagine/index.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+        <div class="cat-nav">
+            <nav>
+                <?php
+                $args = array(
+                    'theme_location' => 'categories'
+                );
+
+                wp_nav_menu($args);
+                ?>
+            </nav>
+        </div>
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+
+        <section class="postpage">
+            <div class="post-image">
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-image'); ?></a>
+            </div>
+            <div class="post">
+                <p class="post-info"><?php
+            
+            $categories = get_the_category();
+            $separator = ", ";
+            $output = '';
+            
+            if ($categories) {
+                
+                foreach ($categories as $category) {
+                
+                    $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
+                    
+                }
+                
+                echo trim($output, $separator);
+                
+            }
+            
+            ?> | <?php the_time('F j, Y'); ?></p>
+    	        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    	                       
+    	        <?php the_content('Read More'); ?>
+            </div>
+    	</section>
+
+        <?php endwhile;
+
+    	else :
+        echo '<p>No content found</p>';
+
+    	endif; 
+    ?>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/imagine/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    
+    var menu = "close";
+
+    $('.menu-btn').click(function() {
+
+        if(menu == "close") {
+            $('.site-nav').css('-webkit-transform', 'translate(0, 0');
+            $('.site-nav').css('-moz-transform', 'translate(0, 0');
+            $('.site-nav').css('transform', 'translate(0, 0');
+            menu = "open";
+        }
+    });
+    
+    $('.close-btn').click(function() {
+
+        if(menu == "open") {
+            $('.site-nav').css('-webkit-transform', 'translate(0, -100%');
+            $('.site-nav').css('-moz-transform', 'translate(0, -100%');
+            $('.site-nav').css('transform', 'translate(0, -100%');
+            menu = "close";
+        }
+    });
+
+    var btt = $('.back-to-top');
+
+    btt.on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 300);
+    });
+
+    var amountScrolled = 250;
+
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > amountScrolled ) {
+            $('.back-to-top').fadeIn(300);
+        } else {
+            $('.back-to-top').fadeOut(300);
+        }
+    });
+});

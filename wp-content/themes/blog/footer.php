d7b9958bd1c7fb1c1b405fa6d3c865be820dfe44
annaosk <a.sebetic@gmail.com>
2015-10-19 12:20:53 +0200

    deleted old files

--- wp-content/themes/blog/footer.php
+++
@@ -1,9 +0,0 @@
-	</div><!--container-->
-		<footer class="clearfix">
-			<p><small><?php bloginfo('name'); ?> - &copy; <?php echo date('Y')?></small></p>
-		</footer>
-
-<?php wp_footer(); ?>
-<a href="#" class="back-to-top"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/scroll-to-top.png"></a>
-</body>
-</html>

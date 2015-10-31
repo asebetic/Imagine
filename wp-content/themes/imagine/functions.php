11f8f441f6738a337af57a9eaed4e46f44a369bb
annaosk <a.sebetic@gmail.com>
2015-10-31 11:00:30 +0100

    deleted image and some changes

--- wp-content/themes/imagine/functions.php
+++ wp-content/themes/imagine/functions.php
@@ -4,16 +4,15 @@
 /**
  *  Blog setup functionality
  */
-function blog_setup() {
-
+function blog_setup() 
+{
+    // Add theme feature title tag
     add_theme_support('title-tag');
 
 	// Add feature image support
 	add_theme_support('post-thumbnails');
-
 	add_image_size('post-image', 420, 420, true);
     add_image_size('single-image', 420, 420, true);
-
 
     // Navigation Menus
     register_nav_menus(array(
@@ -22,34 +21,33 @@ function blog_setup() {
     ));
 }
 
-
 add_action('after_setup_theme', 'blog_setup');
 
 
 /**
  *  Blog scripts
  */
-function blog_scripts() {
-
+function blog_scripts() 
+{
 	wp_enqueue_style('style', get_stylesheet_uri());
-
     wp_enqueue_script( 'blog-js', get_template_directory_uri() . '/js/main.js', array(), '19092015', true );
-
 }
-
 
 add_action('wp_enqueue_scripts', 'blog_scripts');
 
 
 // Excerpt length
-function custom_excerpt_length()
+function custom_excerpt_length() 
 {
     return 25;
 }
 
 add_filter('excerpt_length', 'custom_excerpt_length', 999);
 
-function theme_enqueue_script(){ 
-    wp_enqueue_script('jquery');  
+
+function theme_enqueue_script() 
+{
+    wp_enqueue_script('jquery');
 }
+
 add_action('wp_enqueue_scripts', 'theme_enqueue_script');
--- wp-content/themes/imagine/index.php
+++ wp-content/themes/imagine/index.php
@@ -19,25 +19,19 @@
                 <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-image'); ?></a>
             </div>            
             <div class="post"><p class="post-info"><?php
-            
-            $categories = get_the_category();
-            $separator = ", ";
-            $output = '';
-            
-            if ($categories) {
                 
-                foreach ($categories as $category) {
-                
-                    $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
-                    
-                }
-                
-                echo trim($output, $separator);
-                
-            }
-            
-            ?> | <?php the_time('F j, Y'); ?></p>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>                            
+                $categories = get_the_category();
+                $separator = ", ";
+                $output = '';
+
+                if ($categories) {            
+                    foreach ($categories as $category) {                    
+                        $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;   
+                    }     
+                    echo trim($output, $separator);      
+                }   
+                ?> | <?php the_time('F j, Y'); ?></p>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>                            
                 <?php the_content('Read more'); ?>
             </div>          
     	</section>
@@ -45,6 +39,7 @@
         <?php endwhile;
 
     	else :
+            
         echo '<p>No content found</p>';
 
     	endif; 
